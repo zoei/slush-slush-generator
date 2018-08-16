8ea0e38ba8439c4c3da49cd6cc4c64596f7b190a
hazhihui <hazhihui@hujiang.com>
2018-08-16 15:00:04 +0800

    mod

--- slushfile.js
+++ slushfile.js
@@ -1,34 +1,129 @@
+'use strict';
+
 var gulp = require('gulp'),
-    path = require('path'),
-    install = require('gulp-install'),
-    conflict = require('gulp-conflict'),
-    template = require('gulp-template'),
-    gulpif = require('gulp-if'),
-    inquirer = require('inquirer');
+  install = require('gulp-install'),
+  conflict = require('gulp-conflict'),
+  template = require('gulp-template'),
+  rename = require('gulp-rename'),
+  gulpif = require('gulp-if'),
+  _ = require('underscore.string'),
+  inquirer = require('inquirer'),
+  path = require('path');
+
+
+function format(string) {
+  var username = string.toLowerCase();
+  return username.replace(/\s/g, '');
+}
+
+var defaults = (function () {
+  var workingDirName = path.basename(process.cwd()); 
+  var homeDir; 
+  var osUserName; 
+  var configFile;
+  var user = {};
+
+  if (process.platform === 'win32') {
+    homeDir = process.env.USERPROFILE;
+    osUserName = process.env.USERNAME || path.basename(homeDir).toLowerCase();
+  }
+  else {
+    homeDir = process.env.HOME || process.env.HOMEPATH;
+    osUserName = homeDir && homeDir.split('/').pop() || 'root';
+  }
+
+  configFile = path.join(homeDir, '.gitconfig');
+
+  if (require('fs').existsSync(configFile)) {
+    user = require('iniparser').parseSync(configFile).user;
+  }
+
+  return {
+    appName: workingDirName,
+    userName: osUserName || format((user && user.name) || ''),
+    authorName: (user && user.name) || '',
+    authorEmail: (user && user.email) || ''
+  };
+})();
+
 
 gulp.task('default', function (done) {
-  inquirer.prompt([
-    {
-    	type: 'input',
-    	name: 'name', 
-    	message: 'Give your app a name', 
-    	default: gulp.args.join(' ')                          // 从运行参数中获取值作为 name 的默认值。
-    },
-    {
-    	type: 'confirm',
-    	name: 'install',
-      message: 'Install now?'
-    }
-  ]).then(function (answers) {
-    const DEST = path.join(process.cwd(), `slush-${answers.name}-generator`)
-    console.log('DEST', DEST)
-    gulp.src(['**'], { base: __dirname + '/templates' })      // 使用脚本所在的目录作为相对路径。
-      .pipe(template(answers))                              // Lodash 模板支持，可以方便的在模板中进行插值。
-      .pipe(conflict(DEST))                                 // 当文件冲突时，询问是否覆盖。
-      .pipe(gulp.dest(DEST))                                // 输出到执行命令的当前文件夹中。
-      .pipe(gulpif(answers.install, install()))
-      .on('end', function () {
-        done();                                               // 告知完成。
-      });
-  });
+  var prompts = [{
+    name: 'appName',
+    message: 'What is the name of your slush generator?',
+    default: gulp.args.join(' ') || workingDirName
+  }, {
+    name: 'appDescription',
+    message: 'What is the description?'
+  }, {
+    name: 'appVersion',
+    message: 'What is the version of your slush generator?',
+    default: '0.1.0'
+  }, {
+    name: 'authorName',
+    message: 'What is the author name?',
+    default: defaults.authorName
+  }, {
+    name: 'authorEmail',
+    message: 'What is the author email?',
+    default: defaults.authorEmail
+  }, {
+    name: 'userName',
+    message: 'What is the github username?',
+    default: defaults.userName
+  }, {
+    type: 'list',
+    name: 'license',
+    message: 'Choose your license type',
+    choices: ['MIT', 'BSD'],
+    default: 'MIT'
+  }, {
+    type: 'confirm',
+    name: 'install',
+    message: 'Install?'
+  }, {
+    type: 'confirm',
+    name: 'moveon',
+    message: 'Continue?'
+  }];
+  //Ask
+  inquirer
+    .prompt(prompts)
+    .then(function (answers) {
+
+      if (!answers.moveon) {
+        return done();
+      }
+      answers.appNameSlug = _.slugify('Slush ' + answers.appName);
+      answers.appNameOnly = _.capitalize(answers.appNameSlug.replace('slush-', ''));
+      var d = new Date();
+      answers.year = d.getFullYear();
+      answers.date = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate();
+      var files = [__dirname + '/templates/**'];
+      if (answers.license === 'MIT') {
+        files.push('!' + __dirname + '/templates/LICENSE_BSD');
+      } else {
+        files.push('!' + __dirname + '/templates/LICENSE_MIT');
+      }
+      gulp.src(files)
+        .pipe(template(answers))
+        .pipe(rename(function (file) {
+          if (answers.license === 'MIT') {
+            var mit = file.basename.replace('LICENSE_MIT', 'LICENSE');
+            file.basename = mit;
+          } else {
+            var bsd = file.basename.replace('LICENSE_BSD', 'LICENSE');
+            file.basename = bsd;
+          }
+          if (file.basename[0] === '_') {
+            file.basename = '.' + file.basename.slice(1);
+          }
+        }))
+        .pipe(conflict(`./${gulp.args.join(' ')}`))
+        .pipe(gulp.dest(`./${gulp.args.join(' ')}`))
+        gulpif(answers.install, install())
+        .on('end', function () {
+          done();
+        });
+    });
 });
--- templates/slushfile.js
+++ templates/slushfile.js
@@ -1,31 +1,109 @@
+/*
+ * <%= appNameSlug %>
+ * https://github.com/<%= userName %>/<%= appNameSlug %>
+ *
+ * Copyright (c) <%= year %>, <%= authorName %>
+ * Licensed under the <%= license %> license.
+ */
+
+'use strict';
+
 var gulp = require('gulp'),
-    install = require('gulp-install'),
-    conflict = require('gulp-conflict'),
-    template = require('gulp-template'),
-    gulpif = require('gulp-if'),
-    inquirer = require('inquirer');
+  install = require('gulp-install'),
+  conflict = require('gulp-conflict'),
+  template = require('gulp-template'),
+  rename = require('gulp-rename'),
+  gulpif = require('gulp-if'),
+  _ = require('underscore.string'),
+  inquirer = require('inquirer'),
+  path = require('path');
+
+function format(string) {
+  var username = string.toLowerCase();
+  return username.replace(/\s/g, '');
+}
+
+var defaults = (function () {
+  var workingDirName = path.basename(process.cwd()),
+    homeDir, osUserName, configFile, user;
+
+  if (process.platform === 'win32') {
+    homeDir = process.env.USERPROFILE;
+    osUserName = process.env.USERNAME || path.basename(homeDir).toLowerCase();
+  }
+  else {
+    homeDir = process.env.HOME || process.env.HOMEPATH;
+    osUserName = homeDir && homeDir.split('/').pop() || 'root';
+  }
+
+  configFile = path.join(homeDir, '.gitconfig');
+  user = {};
+
+  if (require('fs').existsSync(configFile)) {
+    user = require('iniparser').parseSync(configFile).user;
+  }
+
+  return {
+    appName: workingDirName,
+    userName: osUserName || format(user.name || ''),
+    authorName: user.name || '',
+    authorEmail: user.email || ''
+  };
+})();
 
 gulp.task('default', function (done) {
-  inquirer.prompt([
-    {
-    	type: 'input',
-    	name: 'name', 
-    	message: 'Give your app a name', 
-    	default: gulp.args.join(' ')                          // 从运行参数中获取值作为 name 的默认值。
-    },
-    {
-    	type: 'confirm',
-    	name: 'install',
-      message: 'Install now?'
-    }
-  ]).then(function (answers) {
-    gulp.src(['**'], { base: __dirname + '/templates' })      // 使用脚本所在的目录作为相对路径。
-      .pipe(template(answers))                              // Lodash 模板支持，可以方便的在模板中进行插值。
-      .pipe(conflict('./'))                                 // 当文件冲突时，询问是否覆盖。
-      .pipe(gulp.dest('./'))                                // 输出到执行命令的当前文件夹中。
-      .pipe(gulpif(answers.install, install()))
-      .on('end', function () {
-        done();                                               // 告知完成。
-      });
-  });
+  var prompts = [{
+    name: 'appName',
+    message: 'What is the name of your project?',
+    default: defaults.appName
+  }, {
+    name: 'appDescription',
+    message: 'What is the description?'
+  }, {
+    name: 'appVersion',
+    message: 'What is the version of your project?',
+    default: '0.1.0'
+  }, {
+    name: 'authorName',
+    message: 'What is the author name?',
+    default: defaults.authorName
+  }, {
+    name: 'authorEmail',
+    message: 'What is the author email?',
+    default: defaults.authorEmail
+  }, {
+    name: 'userName',
+    message: 'What is the github username?',
+    default: defaults.userName
+  }, {
+    type: 'confirm',
+    name: 'install',
+    message: 'Install?'
+  }, {
+    type: 'confirm',
+    name: 'moveon',
+    message: 'Continue?'
+  }];
+  //Ask
+  inquirer
+    .prompt(prompts)
+    .then(function (answers) {
+      if (!answers.moveon) {
+        return done();
+      }
+      answers.appNameSlug = _.slugify(answers.appName);
+      gulp.src(__dirname + '/templates/**')
+        .pipe(template(answers))
+        .pipe(rename(function (file) {
+          if (file.basename[0] === '_') {
+            file.basename = '.' + file.basename.slice(1);
+          }
+        }))
+        .pipe(conflict(`./${gulp.args.join(' ')}`))
+        .pipe(gulp.dest(`./${gulp.args.join(' ')}`))
+        gulpif(answers.install, install())
+        .on('end', function () {
+          done();
+        });
+    });
 });
